9dbf472fc40627a9845730d0fa60f7a895ff4840
Yuliia00 <skaskevich2000@gmail.com>
2023-02-21 18:59:00 +0100

    hw_6

---
+++ script.js
@@ -0,0 +1,4 @@
+const secondsInHours = 3600
+let numberHours = parseInt(prompt("Enter number of hours:", '5'));
+const numberSeconds = numberHours * secondsInHours
+alert(`Number of seconds in ${numberHours} hours: ${numberSeconds}`);
